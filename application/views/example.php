35606c1bf6bac8872234e8460417f90ccc4037eb
Robert McGregor <me@robertmcgregor.ca>
2014-03-01 15:23:40 -0500

    upadate imageCRUD controller

--- application/views/example.php
+++ application/views/example.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8" />
 <?php 
 foreach($css_files as $file): ?>
-	<link type="text/css" rel="stylesheet" href="<?php echo '~'.$file; ?>" />
+	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
 <?php endforeach; ?>
 <?php foreach($js_files as $file): ?>
 	<script src="<?php echo $file; ?>"></script>
