d6882d28e8168d1edf26684bef8ad96ebed84afc
Robert McGregor <me@robertmcgregor.ca>
2014-03-01 22:06:40 -0500

    upadate imageCRUD controller

--- application/controllers/photos.php
+++ application/controllers/photos.php
@@ -15,24 +15,18 @@ class Photos extends MY_Controller {
 
 		$this->_load_css(array(CSS."photos.css"));
 
-		/*
-		$this->_render("photos/view_photos","example"));
-		*/
-
-		$this->load->library('image_CRUD');
-
-	    $image_crud = new image_CRUD();
-		 
-	    $image_crud->set_table('example_2');
-		 
+		$image_crud = new image_CRUD();
+	
 		$image_crud->set_primary_key_field('id');
 		$image_crud->set_url_field('url');
-		$image_crud->set_ordering_field('priority')
+		$image_crud->set_title_field('title');
+		$image_crud->set_table('wedding_album')
+		->set_ordering_field('priority')
 		->set_image_path('assets/uploads');
-
+			
 		$output = $image_crud->render();
-
-		$this->_render(array("photos/view_photos","example"),NULL);
+	
+		$this->_render(array("photos/view_photos"),NULL);
 
 	}	 
 	
